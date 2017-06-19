98f2c1403293b260e298bf926570744ac7bf30ef
bruno.krebs <krebs.bruno@gmail.com>
2017-06-19 16:53:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const morgan = require('morgan');
+
+// creates Express' app
+const app = express();
+
+// adding HTTP request logger middleware
+app.use(morgan('tiny'));
+
+app.use(bodyParser.text());
+
+// initial list of items
+let items = [
+	'Feed the dogs',
+	'Mow the lawn',
+	'Buy pizza'
+];
+
+// returns all items (public endpoint)
+app.get('/', function(req, res){
+	res.send(items);
+});
+
+// Auth0's middleware to secure endpoints
+const jwtCheck = jwt({
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://krebshaus.auth0.com/.well-known/jwks.json`
+	}),
+	audience: 'http://kotlin-test.com',
+	issuer: `https://krebshaus.auth0.com/`,
+	algorithms: ['RS256']
+});
+
+// accepts new items (secured endpoint)
+app.post('/', jwtCheck, function (req, res) {
+	items.push(req.body);
+	res.sendStatus(200)
+});
+
+app.listen(process.env.PORT || 8080);
